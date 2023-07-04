03c76b93d1777d7162d5fdf0660c8a37f46f6c30
Dipa <dipamitro63@gmail.com>
2023-07-04 19:25:21 +0600

    commit1

---
+++ javascript.js
@@ -0,0 +1,51 @@
+function chang1(){
+    document.getElementById('doller').innerHTML='Tk.6382';
+    document.getElementById('doller').style.fontSize='40px';
+    document.getElementById('doller').style.transition='all 1s';
+}
+function chang2(){
+    document.getElementById('doller').innerHTML='Rs.4843';
+    document.getElementById('doller').style.fontSize='40px';
+    document.getElementById('doller').style.transition='all 1s';
+
+}
+function dipa22(){
+    document.getElementById('doller2').innerHTML='Rs.8127';
+    document.getElementById('doller2').style.fontSize='40px';
+    document.getElementById('doller').style.transition='all 2s';
+}
+function dipa11(){
+    document.getElementById('doller2').innerHTML='Tk.10640';
+    document.getElementById('doller2').style.fontSize='40px';
+    document.getElementById('doller2').style.transition='all 2s';
+}
+function dipa222(){
+    document.getElementById('doller3').innerHTML='Rs.16337 ';
+    document.getElementById('doller3').style.fontSize='40px';
+    document.getElementById('doller3').style.transition='all 1s';
+}
+function dipa111(){
+    document.getElementById('doller3').innerHTML='Tk.21389';
+    document.getElementById('doller3').style.fontSize='40px';
+    document.getElementById('doller3').style.transition='all 1s';
+}
+
+
+function tusday() {
+    document.getElementById("for_invisi2").style.display = "none";
+  }
+
+
+           
+
+
+
+
+
+ 
+
+
+
+
+
+  
